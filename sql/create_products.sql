6042c294bf03a18ffd986901acb7830f7214d793
Bob Kwiencien <bkwiencien@gmail.com>
2017-10-03 09:15:42 -0400

    build products

--- sql/create_products.sql
+++ sql/create_products.sql
@@ -1,7 +1,7 @@
  create table products (
  item_id integer  not null auto_increment,
  product_name varchar(25) not null,
- scientic_name varchar(50),
+ scientific_name varchar(50),
  department_name varchar(12),
  price float(4,2) not null,
  stock_quantity integer,
---
+++ sql/populate_products.sql
@@ -0,0 +1,6 @@
+insert into products (product_name,scientific_name,department_name,price,stock_quantity) values('Sugar Apple','Annona squamosa','fruit',2.00,12);
+insert into products (product_name,scientific_name,department_name,price,stock_quantity) values('Mammee Apple','Mammea americana','fruit',2.50,20);	
+insert into products (product_name,scientific_name,department_name,price,stock_quantity) values('Cherymoya','Annona cherimola','fruit',3.00,10);	
+insert into products (product_name,scientific_name,department_name,price,stock_quantity) values('Platonia','Platonia insignis','fruit',3.00,10);	
+insert into products (product_name,scientific_name,department_name,price,stock_quantity) values('Breadfruit','Artocarpus altilis','fruit',3.00,5); 	
+commit;
