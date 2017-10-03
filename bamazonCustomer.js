f193218ae80771ec6bf692ec69ac97c9ed90df77
Bob Kwiencien <bkwiencien@gmail.com>
2017-10-03 12:29:16 -0400

    inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var currentInventory = [];
 var connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
@@ -9,7 +10,33 @@ var connection = mysql.createConnection({
 });
 connection.connect(function(err){
         if(err){throw err;}
-        selectAll();
-        console.log("connected as id " + connection.threadId);
+        takeInventory();
+        endItAll();
 
 });
+function takeInventory() {
+	    currentInventory = [];
+        connection.query("select * from products order by item_id",function(err,results){
+                if(err){throw err;}
+               for (j=0;j<results.length;j++) {
+                	var data = {
+                		item_id:       results[j].item_id,
+                		product_name: results[j].product_name,
+                		department_name: results[j].department_name,
+                		price:          results[j].price
+                	}
+                    currentInventory.push(data); 
+                }
+                for (j=0;j<currentInventory.length;j++) {
+                	var str = "item_id " + currentInventory[j].item_id + " ";
+                	    str = str + "product name " + currentInventory[j].product_name + " ";
+                	    str = str + "depertment name " + currentInventory[j].department_name + " ";
+                	    str = str + "price " + currentInventory[j].price + "\n";
+                	    console.log(str);
+                }
+               
+        });
+    }
+    function endItAll() {
+    	connection.end();
+    }
